6d08bd0d7db9a0e4ccc86e24bc81300bab10f338
bociwess0 <bogdandjordjevic2000@gmail.com>
2026-03-23 21:35:00 +0100

    building sidebar

---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <div className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-[48%] lg:flex-col lg:justify-between lg:py-24">
+      <h1 className="text-4xl font-bold">Bogdan Djordjevic</h1>
+      <div className="text-2xl font-medium">Frontend Developer</div>
+    </div>
+  );
+}
