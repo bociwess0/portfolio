1c4c7dfc73c461565cf1ccb1f05ac318ecabe732
bociwess0 <bogdandjordjevic2000@gmail.com>
2026-03-25 21:52:53 +0100

    adding appropriate sentence about me

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -10,7 +10,7 @@ export default function Sidebar() {
         <h1 className="text-4xl font-bold mb-2 leading-relaxed ">Bogdan Djordjevic</h1>
         <div className="text-2xl font-medium mb-3">Frontend Developer</div>
         <div className="text-lg text-slate-400 max-w-xs">
-          I build accessible, pixel-perfect digital experiences for the web.
+          I build scalable, full-stack web applications with a focus on performance and user experience.
         </div>
 
         <SidebarTabs headerTabs={HeaderTabs} />
