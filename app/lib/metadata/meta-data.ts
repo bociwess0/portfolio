cc156d9c06b6afdfaf1b4c235b399995fcfc66ab
bociwess0 <bogdandjordjevic2000@gmail.com>
2026-03-29 20:02:28 +0200

    fixing metadata

---
+++ app/lib/metadata/meta-data.ts
@@ -0,0 +1,25 @@
+import { Metadata } from "next";
+
+export const siteMetadata: Metadata = {
+  title: "Bogdan Djordjevic | Frontend Developer",
+  description:
+    "Frontend developer specializing in scalable, high-performance web applications using Next.js, TypeScript, and modern technologies.",
+
+  openGraph: {
+    title: "Bogdan Djordjevic Portfolio",
+    description:
+      "Building scalable, full-stack web applications with focus on performance and user experience.",
+    url: "https://bogdan-djordjevic-portfolio.vercel.app",
+    siteName: "Bogdan Portfolio",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Bogdan Djordjevic Portfolio",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
